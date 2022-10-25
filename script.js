d064e5e17ecbde7a013912a4c133f177e411a585
Ha Young Roh <hyroh795@gmail.com>
2022-10-24 20:27:08 -0400

    adjust html,js,css

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+function bigImg(x) {
+    x.style.height = "200x";
+    x.style.width = "200px";
+}
+
+function normalImg(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+}
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello, Jin";
+}
